a75bfd4e82e923e94acd40cae3fdb445e27fadd8
Heaven Ike <H.Ike1@nuigalway.ie>
2022-05-24 16:11:19 +0100

    basic Formt

--- E Commerce POS/Form1.cs
+++ E Commerce POS/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace E_Commerce_POS
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void PanelTitle_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
